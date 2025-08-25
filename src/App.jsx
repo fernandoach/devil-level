7ea55eb807e9239a8aa37303c396ee66b349740c
Fernando A. <fernandoach333@gmail.com>
2025-08-24 22:03:33 -0500

    Refactor App component structure and wrap in BrowserRouter for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { Link, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>DEVIL LEVEL</h1>
+      <nav>
+        <Link to={'/'}>
+          <h1>DEVIL LEVEL</h1>
+        </Link>
+        <ul>
+          <li>
+            menu
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+
+      </Routes>
+
     </>
   )
 }

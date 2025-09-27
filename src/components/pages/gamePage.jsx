23d856324fdef299bb5e9b133cd4fc42ae7ce2d6
ErickAbanto <aeriatab@gmail.com>
2025-09-26 21:56:58 -0500

    feat(ui): agregar estilos y estructura a la página del juego

--- src/components/pages/gamePage.jsx
+++ src/components/pages/gamePage.jsx
@@ -1,11 +1,21 @@
 import './gamePage.css'
 
-function GamePage(){
+function GamePage() {
+  const gameTitle = "APOCALYPSE-le"
+
   return (
-    <>
-      <h1>Pagina del juego</h1>
-      
-    </>
+    <section className='game__section'>
+      <img src="./INSTRUCCIONES.png" alt="game top" className="img__game top" />
+      <div className='container_game'>
+        <h2 className='game__title'>{gameTitle}</h2>
+        <div className="container__Eye">
+          <img src="/JUEGO.png" alt="Eye left" className="img__eye left"/>
+          <img src="/JUEGO.png" alt="Eye right" className="img__eye right"/>
+        </div>
+        <a href='#' className="button__game">Jugar</a>
+      </div>
+      <img src="./INSTRUCCIONES.png" alt="game below" className="img__game below" />
+    </section>
   )
 }
 
